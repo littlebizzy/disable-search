e0914043df39c7123a4b5a59ddc46c9d7f73208b
Jesse Nickles <jesse@littlebizzy.com>
2017-09-26 10:36:20 -0700

    Add files via upload

---
+++ disable-search.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: Disable Search
+Plugin URI: https://www.littlebizzy.com/plugins/disable-search
+Description: Disable Search
+Version: 1.0
+Author: LittleBizzy
+Author URI: https://www.littlebizzy.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+Copyright 2016 by LittleBizzy
+
+*/
+
+
+/* Initialization */
+
+// Avoid script calls via plugin URL
+if (!function_exists('add_action'))
+	die;
+
+// Quick context check
+if (is_admin())
+	return;
+
+// This plugin constants
+define('DSBSRC_FILE', __FILE__);
+define('DSBSRC_PATH', dirname(DSBSRC_FILE));
+define('DSBSRC_VERSION', '1.0');
+
+// WP parse query hook
+add_action('parse_query', 'dsbsrc_parse_query', 0);
+
+// WP parse query handler
+function dsbsrc_parse_query(&$wp_query) {
+	if ($wp_query->is_main_query() && $wp_query->is_search) {
+		require_once(DSBSRC_PATH.'/disable-search-404.php');
+		DSBSRC_404::alter($wp_query);
+	}
+}
