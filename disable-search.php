af42e5e26296d4f6293c4eb2e144fceef2166e19
Jesse Nickles <jesse@littlebizzy.com>
2025-03-18 23:12:33 +0700

    Update disable-search.php

--- disable-search.php
+++ disable-search.php
@@ -26,3 +26,34 @@ add_filter( 'gu_override_dot_org', function( $overrides ) {
     $overrides[] = 'disable-search/disable-search.php';
     return $overrides;
 }, 999 );
+
+// disable search queries
+function disable_search_query($query) {
+    if (!is_admin() && $query->is_search) {
+        $query->is_search = false;
+        $query->set_404();
+        status_header(404);
+        nocache_headers();
+    }
+}
+add_action('parse_query', 'disable_search_query');
+
+// remove search form
+function disable_search_form($form) {
+    return '';
+}
+add_filter('get_search_form', 'disable_search_form');
+
+// disable search widget
+function disable_search_widget() {
+    unregister_widget('WP_Widget_Search');
+}
+add_action('widgets_init', 'disable_search_widget');
+
+// remove search from admin bar
+function disable_search_admin_bar($wp_admin_bar) {
+    $wp_admin_bar->remove_node('search');
+}
+add_action('admin_bar_menu', 'disable_search_admin_bar', 999);
+
+// Ref: ChatGPT
